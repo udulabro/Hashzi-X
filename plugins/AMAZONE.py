f9e6d1da8e0f2c404a4e2654c4b72f5fadfa9098
udulabro <93195741+udulabro@users.noreply.github.com>
2021-11-19 22:37:11 +0530

    Update AMAZONE.py

--- plugins/AMAZONE.py
+++ plugins/AMAZONE.py
@@ -1,7 +1,7 @@
-/* Copyright (C) 2021 TENUX-Neotro.
+/* Copyright (C) 2021 UDULA-BRO.
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
-NEOTROX - TEENUHX
+MRBOT - UDULABRO
 */
 
 481c863d-be29-4b7f-854c-ea859ea2dacff281ff67-7b39-470a-b7b0-12dc933d8c11481c863d-be29-4b7f-854c-ea859ea2dacff281ff67-7b39-470a-b7b0-12dc933d8c11481c863d-be29-4b7f-854c-ea859ea2dacff281ff67-7b39-470a-b7b0-12dc933d8c11481c863d-be29-4b7f-854c-ea859ea2dacf
